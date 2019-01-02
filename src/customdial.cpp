0e0553ae389527fa9c34ed09ff30a73aeed308f9
Mario Stephan <mstephan@shared-files.de>
2019-01-02 01:16:43 +0100

    usage of CustomDial

---
+++ src/customdial.cpp
@@ -0,0 +1,6 @@
+#include "customdial.h"
+
+CustomDial::CustomDial()
+{
+
+}
---
+++ src/customdial.h
@@ -0,0 +1,11 @@
+#ifndef CUSTOMDIAL_H
+#define CUSTOMDIAL_H
+
+
+class CustomDial
+{
+public:
+    CustomDial();
+};
+
+#endif // CUSTOMDIAL_H
